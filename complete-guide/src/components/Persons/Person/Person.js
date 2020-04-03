db55336e8988d778c6c09984d7f68a867b0d888d
Doug Sewell <DouglasASewell@gmail.com>
2020-04-02 22:18:25 -0400

    Created wrapper class and added it to the Person component

--- complete-guide/src/components/Persons/Person/Person.js
+++ complete-guide/src/components/Persons/Person/Person.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
+import Aux from '../../../hoc/Aux';
 import classes from './Person.module.css';
+
 
 class Person extends Component {
     render() {
         console.log('[Person.js] rendering...');
         return (
-            <div className={classes.Person}>
+            <Aux className={classes.Person}>
                 <p className="personText" onClick={this.props.click}>My name {this.props.name}! I am {this.props.age}</p>
                 <p>{this.props.children}</p>
                 <input
                     onChange={this.props.change}
                     type="text"
                     value={this.props.name} />
-            </div>
+            </Aux>
         );
     }
 }
