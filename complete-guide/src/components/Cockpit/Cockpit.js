9e64dae8bf5440fe5216d9483a9e3c94dc585ba1
Doug Sewell <douglasasewell@gmail.com>
2020-04-07 23:03:09 -0400

    Implemented context to components to streamline passing props

--- complete-guide/src/components/Cockpit/Cockpit.js
+++ complete-guide/src/components/Cockpit/Cockpit.js
@@ -1,10 +1,12 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useContext } from 'react';
 import classes from './cockpit.module.css';
 import AuthContext from '../../context/auth-context';
 
 const Cockpit = (props) => {
 
   const toggleBtnRef = useRef(null);
+  const authContext = useContext(AuthContext);
+  console.log(authContext.authenticated);
 
   useEffect(() => {
     console.log('[Cockpit.js] useEffect');
@@ -54,10 +56,7 @@ const Cockpit = (props) => {
         className={btnClass}
         onClick={props.clicked}>Toggle names
       </button>
-      <AuthContext.Consumer>
-        {(context) => <button onClick={context.login}>Log in</button>}
-      </AuthContext.Consumer>
-
+        { <button onClick={authContext.login}>Log in</button>}
     </div>
   )
 }
--- complete-guide/src/components/Persons/Person/Person.js
+++ complete-guide/src/components/Persons/Person/Person.js
@@ -12,17 +12,18 @@ class Person extends Component {
         this.inputElementRef = React.createRef();
     }
 
+    static contextType = AuthContext;
+
     componentDidMount() {
        this.inputElementRef.current.focus();
+       console.log(this.context.authenticated);
     }
 
     render() {
         console.log('[Person.js] rendering...');
         return (
             <Aux>
-                <AuthContext.Consumer>
-                    {(context) => context.authenticated ? <p>Authenticated!</p> : <p>Please log in</p> }
-                </AuthContext.Consumer>
+                    {this.context.authenticated ? <p>Authenticated!</p> : <p>Please log in</p> }
                 <p className="personText" onClick={this.props.click}>My name {this.props.name}! I am {this.props.age}</p>
                 <p>{this.props.children}</p>
                 <input
