85e38d0025da28559eb07b0f886eca57ce38e995
Doug Sewell <DouglasASewell@gmail.com>
2020-04-19 21:30:51 -0400

    Added controls for adding and removing burger ingredients. Not functional yet, but UI is there.

--- burger-app/src/components/Burger/BuildControls/BuildControls.js
+++ burger-app/src/components/Burger/BuildControls/BuildControls.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import classes from './BuildControls.module.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controsl = [
+    {label: 'Salad', stype: 'salad'},
+    {label: 'Bacon', stype: 'bacon'},
+    {label: 'Cheese', stype: 'cheese'},
+    {label: 'Meat', stype: 'salad'}]
 
 const buildControls = (props) => (
-    <div>
-        
+    <div className={classes.buildControls}>
+        {controsl.map(ctrl => (
+            <BuildControl key={ctrl.label} label={ctrl.label} />
+        ))}
     </div>
 );
 
