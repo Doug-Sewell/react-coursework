75d2b9fd9fce1aa6df2763be1cc460d361e6406a
Doug Sewell <DouglasASewell@gmail.com>
2020-04-25 22:36:26 -0400

    Added new component for burger ingredients

--- burger-app-4/src/App.js
+++ burger-app-4/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout/Layout';
+import BurgerBuilder from './components/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
   render() {
     return (
       <Layout>
-        <p>Test</p>
+        <BurgerBuilder />
       </Layout>
       );
   }
