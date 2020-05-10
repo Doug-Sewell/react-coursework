fb28bf265bbf5b31d391da2baa2afdef81f64e4f
Doug Sewell <DouglasASewell@gmail.com>
2020-05-09 22:22:43 -0400

    Navigational drawer added.

--- burger-app-4/src/components/Layout/Layout.js
+++ burger-app-4/src/components/Layout/Layout.js
@@ -8,7 +8,7 @@ class Layout extends Component {
 
 
     state = {
-        showSideDrawer: true
+        showSideDrawer: false
     }
 
 
@@ -16,15 +16,20 @@ class Layout extends Component {
         this.setState({ showSideDrawer: false });
     }
 
+    sideDrawerToggleHandler = () => {
+        this.setState(prevState => {
+            return { showSideDrawer: !prevState.showSideDrawer }
+        });
+    }
 
     render() {
-        
+
         return (
             <Aux>
-                <Toolbar />
-                <SideDrawer 
-                open={this.state.showSideDrawer}
-                closed={this.sideDrawerClosedHandler} />
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler} />
                 <main className={classes.Content}>
                     {this.props.children}
                 </main>
