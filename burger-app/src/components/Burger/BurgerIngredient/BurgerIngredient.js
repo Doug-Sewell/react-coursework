4cf76169f1d8b5a436e7de68d98e092af046f936
Doug Sewell <DouglasASewell@gmail.com>
2020-05-11 17:18:30 -0400

    Removed older verisons of burger app. Renamed burger-app-4 folder to just burger-app.

--- burger-app/src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ burger-app/src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -8,7 +8,7 @@ class BurgerIngredient extends Component {
 
         switch (this.props.type) {
             case ('bread-bottom'):
-                ingredient = <div className={classes.BreadBottom}></div>
+                ingredient = <div className={classes.BreadBottom}></div>;
                 break;
             case ('bread-top'):
                 ingredient = (
@@ -19,23 +19,25 @@ class BurgerIngredient extends Component {
                 );
                 break;
             case ('meat'):
-                ingredient = <div className={classes.Meat}></div>
+                ingredient = <div className={classes.Meat}></div>;
                 break;
             case ('cheese'):
-                ingredient = <div className={classes.Cheese}></div>
+                ingredient = <div className={classes.Cheese}></div>;
                 break;
             case ('bacon'):
-                ingredient = <div className={classes.Bacon}></div>
+                ingredient = <div className={classes.Bacon}></div>;
                 break;
             case ('salad'):
-                ingredient = <div className={classes.Salad}></div>
+                ingredient = <div className={classes.Salad}></div>;
                 break;
             default:
                 ingredient = null;
         }
+        
         return ingredient;
+        
     }
-}
+};
 
 BurgerIngredient.propTypes = {
     type: PropTypes.string.isRequired
