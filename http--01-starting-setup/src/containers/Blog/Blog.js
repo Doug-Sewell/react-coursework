7e84d843261d3bfc9069ffe3f4cb2419e205499a
Doug Sewell <DouglasASewell@gmail.com>
2020-05-13 18:37:15 -0400

    Fetches data from server and renders post previews to the DOM

---
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import Axios from 'axios';
+
+import Post from '../../components/Post/Post';
+import FullPost from '../../components/FullPost/FullPost';
+import NewPost from '../../components/NewPost/NewPost';
+import './Blog.css';
+
+class Blog extends Component {
+
+    state = {
+        posts: []
+    }
+
+
+    componentDidMount() {
+        Axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                console.log(response);
+                this.setState({posts: response.data});
+            });
+    }
+    
+    render () {
+
+        const posts = this.state.posts.map(post => <Post title={post.title} key={post.id} />);
+
+        return (
+            <div>
+                <section className="Posts">
+                    {posts}
+                </section>
+                <section>
+                    <FullPost />
+                </section>
+                <section>
+                    <NewPost />
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Blog;
