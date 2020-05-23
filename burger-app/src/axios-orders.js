b39b40a668eb19dd2ca741689207b2826f02a49c
Doug Sewell <DouglasASewell@gmail.com>
2020-05-22 21:07:47 -0400

    Implemented error handling, though at the moment the app has an error. Not in a working state. Need to fix.

--- burger-app/src/axios-orders.js
+++ burger-app/src/axios-orders.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL:'https://react-my-burger-f6e19.firebaseio.com/'
-    //baseURL: '********ADD FIREBASE URL HERE************'
+    baseURL: '********ADD FIREBASE URL HERE************'
 });
 
 export default instance;
