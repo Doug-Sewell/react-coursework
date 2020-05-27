74d4e77daa440f7e7d58b505bc339449af68226c
Doug Sewell <DouglasASewell@gmail.com>
2020-05-26 22:50:08 -0400

    Utilized routing to load the Posts component

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './Blog.css';
 import Posts from './Posts/Posts';
+import { Route } from 'react-router-dom';
 class Blog extends Component {
 
     render() {
@@ -15,7 +16,10 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Posts />
+                {/*<Route path="/" exact render={() => <h1>Home</h1>} />
+                <Route path="/" render={() => <h1>Home2</h1>} />*/ }
+                <Route path="/" exact component={Posts} />
+
             </div>
         );
     }
